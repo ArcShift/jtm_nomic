90d2ad322126963e0ab8328ff009766c4cdf4ea0
ArcShift <ma.kaafi@yahoo.co.id>
2020-06-02 08:10:08 +0700

    kelola umkm

--- application/views/phpmu-marketpro/reseller/mod_reseller/kelola_umkm.php
+++ application/views/phpmu-marketpro/reseller/mod_reseller/kelola_umkm.php
@@ -1,2 +1,151 @@
-KELOLA UMKM
+<div class='col-12 notif mt-5'>
+    <ul class="nav nav-tabs" id="myTab" role="tablist">
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#tab-e-commerce">E-Commerce</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#tab-produksi">Produksi</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#tab-perbankan">Perbankan</a>
+        </li>
+    </ul>
+    <div class="tab-content" id="myTabContent"><br>
+        <div class="tab-pane fade show active" id="tab-e-commerce">
+            <div class="row">
+                <div class="col-3">
+                    <a class="nav-link">Pengaturan Toko</a>
+                    <a class="nav-link">Daftar Produk</a>
+                    <a class="nav-link">Alamat COD</a>
+                    <a class="nav-link">Order Masuk</a>
+                    <a class="nav-link">Tarik Dana</a>
+                </div>
+                <div class="col-9">
+                    <div class="col-xl-9 col-lg-9 col-md-12 col-sm-12 col-12 ">
+                        <figure class="ps-block--vendor-status biodata">
+                            <div class="form-group row" style="margin-bottom:5px">
+                                <label class="col-sm-3 col-form-label" style="margin-bottom:1px">Nama Reseller</label>
+                                <div class="col-sm-9">
+                                    ArcShop
+                                </div>
+                            </div>
 
+                            <div class="form-group row" style="margin-bottom:5px">
+                                <label class="col-sm-3 col-form-label" style="margin-bottom:1px">Nama Daerah</label>
+                                <div class="col-sm-9">
+
+                                </div>
+                            </div>
+
+                            <div class="form-group row" style="margin-bottom:5px">
+                                <label class="col-sm-3 col-form-label" style="margin-bottom:1px">Alamat Lengkap</label>
+                                <div class="col-sm-9">
+                                    Jl. Mawar
+                                </div>
+                            </div>
+
+                            <div class="form-group row" style="margin-bottom:5px">
+                                <label class="col-sm-3 col-form-label" style="margin-bottom:1px">Kontak UMKM</label>
+                                <div class="col-sm-9">
+                                    089606696705
+                                </div>
+                            </div>
+
+                            <div class="form-group row" style="margin-bottom:5px">
+                                <label class="col-sm-3 col-form-label" style="margin-bottom:1px">Keterangan</label>
+                                <div class="col-sm-9">
+                                    toko serba ada
+                                </div>
+                            </div>
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="tab-pane fade" id="tab-produksi">
+            <div class="row">
+                <div class="col-3">
+                    <a class="nav-link">Bahan Produksi</a>
+                    <a class="nav-link">Manajemen Stok</a>
+                    <a class="nav-link">Statistik Produksi</a>
+                    <a class="nav-link">Statistik Penjualan</a>
+                    <a class="nav-link">Keuangan Sederhana</a>
+                </div>
+                <div class="col-9">
+
+                </div>
+            </div>
+        </div>
+        <div class="tab-pane fade" id="tab-perbankan">
+            <div class="row">
+                <div class="col-3">
+                    <a class="nav-link">Bursa Pinjaman Modal</a>
+                    <a class="nav-link">Daftar Pengajuan Pinjaman</a>
+                </div>
+                <div class="col-9">
+                    <div class="row">
+                        <div class="col-9">
+                            <div class="row">
+                                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12 ">
+                                    <article class="ps-block--store-2">
+                                        <div class="ps-block__content bg--cover" data-background="https://localhost/jatimnomic/asset/images/default-store-banner.png" style="background: rgba(0, 0, 0, 0) url(&quot;https://localhost/jatimnomic/asset/images/default-store-banner.png&quot;) repeat scroll 0% 0%;">
+                                            <figure>
+                                                <img style="width:20px; margin-right:5px; float:left" src="https://localhost/jatimnomic/asset/foto_produk/star1.png"><h4>Bank Mandiri</h4>
+                                            </figure>
+                                        </div>
+                                        <div class="ps-block__author"><a class="ps-block__user" href="#"><img src="https://localhost/jatimnomic/asset/foto_user/toko.jpg" alt=""></a><a class="ps-btn" href="https://localhost/jatimnomic/produk/produk_reseller/41">Ajukan Pinjaman</a></div>
+                                    </article>
+                                </div><div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12 ">
+                                    <article class="ps-block--store-2">
+                                        <div class="ps-block__content bg--cover" data-background="https://localhost/jatimnomic/asset/images/default-store-banner.png" style="background: rgba(0, 0, 0, 0) url(&quot;https://localhost/jatimnomic/asset/images/default-store-banner.png&quot;) repeat scroll 0% 0%;">
+                                            <figure>
+                                                <h4>Bank BRI</h4>
+                                            </figure>
+                                        </div>
+                                        <div class="ps-block__author"><a class="ps-block__user" href="#"><img src="https://localhost/jatimnomic/asset/foto_user/toko.jpg" alt=""></a><a class="ps-btn" href="https://localhost/jatimnomic/produk/produk_reseller/40">Ajukan Pinjaman</a></div>
+                                    </article>
+                                </div><div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12 ">
+                                    <article class="ps-block--store-2">
+                                        <div class="ps-block__content bg--cover" data-background="https://localhost/jatimnomic/asset/images/default-store-banner.png" style="background: rgba(0, 0, 0, 0) url(&quot;https://localhost/jatimnomic/asset/images/default-store-banner.png&quot;) repeat scroll 0% 0%;">
+                                            <figure>
+                                                <h4>Bank BCA</h4>
+                                            </figure>
+                                        </div>
+                                        <div class="ps-block__author"><a class="ps-block__user" href="#"><img src="https://localhost/jatimnomic/asset/foto_user/toko.jpg" alt=""></a><a class="ps-btn" href="https://localhost/jatimnomic/produk/produk_reseller/39">Ajukan Pinjaman</a></div>
+                                    </article>
+                                </div><div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12 ">
+                                    <article class="ps-block--store-2">
+                                        <div class="ps-block__content bg--cover" data-background="https://localhost/jatimnomic/asset/images/default-store-banner.png" style="background: rgba(0, 0, 0, 0) url(&quot;https://localhost/jatimnomic/asset/images/default-store-banner.png&quot;) repeat scroll 0% 0%;">
+                                            <figure>
+                                                <h4>KSP Sejahtera</h4>
+                                            </figure>
+                                        </div>
+                                        <div class="ps-block__author"><a class="ps-block__user" href="#"><img src="https://localhost/jatimnomic/asset/foto_user/Desert.jpg" alt=""></a><a class="ps-btn" href="https://localhost/jatimnomic/produk/produk_reseller/38">Ajukan Pinjaman</a></div>
+                                    </article>
+                                </div><div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12 ">
+                                    <article class="ps-block--store-2">
+                                        <div class="ps-block__content bg--cover" data-background="https://localhost/jatimnomic/asset/images/default-store-banner.png" style="background: rgba(0, 0, 0, 0) url(&quot;https://localhost/jatimnomic/asset/images/default-store-banner.png&quot;) repeat scroll 0% 0%;">
+                                            <figure>
+                                                <h4>Bank Mega</h4>
+                                            </figure>
+                                        </div>
+                                        <div class="ps-block__author"><a class="ps-block__user" href="#"><img src="https://localhost/jatimnomic/asset/foto_user/toko.jpg" alt=""></a><a class="ps-btn" href="https://localhost/jatimnomic/produk/produk_reseller/37">Ajukan Pinjaman</a></div>
+                                    </article>
+                                </div><div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12 ">
+                                    <article class="ps-block--store-2">
+                                        <div class="ps-block__content bg--cover" data-background="https://localhost/jatimnomic/asset/images/default-store-banner.png" style="background: rgba(0, 0, 0, 0) url(&quot;https://localhost/jatimnomic/asset/images/default-store-banner.png&quot;) repeat scroll 0% 0%;">
+                                            <figure>
+                                                <h4>Bank Jatim</h4>
+                                            </figure>
+                                        </div>
+                                        <div class="ps-block__author"><a class="ps-block__user" href="#"><img src="https://localhost/jatimnomic/asset/foto_user/030EBF7C-CE14-40CF-8463-F0329D51F59C.jpeg" alt=""></a><a class="ps-btn" href="https://localhost/jatimnomic/produk/produk_reseller/36">Ajukan Pinjaman</a></div>
+                                    </article>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
